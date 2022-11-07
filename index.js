764db11f58c07205183e4f8fcce711913a8fe1b9
Hassan Razak <hassanrazak1991@gmail.com>
2022-11-07 15:22:17 -0500

    commit tree e3492f2c1d2f6ee30eef89aaaba151bad36d3e52

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ["Milo", "Otis","Garfield"];
+
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+
+//prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
+
+function appendCat(name){ 
+    const copyOfCats = [...cats,name];
+    return copyOfCats;
+}
+
+function prependCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(name);
+    return copyOfCats;
+}
+
+function removeLastCat(){
+    const copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+}
+
+function removeFirstCat(){
+    const copyOfCats = [...cats];
+    copyOfCats.shift();
+    return copyOfCats;
+}
+
